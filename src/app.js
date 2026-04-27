ff2709021bf2c0ab15ad7957e1208acc5279a86c
Lucas Junqueira Bastos da Silva <lucasjbastos26@gmail.com>
2026-04-27 05:12:02 -0300

    feat: page-catalog

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const homeRoutes = require('./routes/homeRoutes');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/media', express.static(path.join(__dirname, '..', 'media')));
+app.use('/assets/games', express.static(path.join(__dirname, '..', 'media', 'local-catalog')));
 app.use('/', homeRoutes);
 
 app.use((req, res) => {
